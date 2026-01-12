7880fe823b794a679785e8aa6d0314cb5c9e4ea6
김순용 <npt1237@gmail.com>
2026-01-12 21:34:52 +0900

    .

--- src/test/java/com/quickticket/quickticket/QuickticketApplicationTests.java
+++ src/test/java/com/quickticket/quickticket/QuickticketApplicationTests.java
@@ -1,7 +1,16 @@
 package com.quickticket.quickticket;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class QuickticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+        assertEquals(1 + 1, 2);
+	}
+
 }
