0e549d829e35f7f16b03c88bee9f149cd64f542b
김순용 <npt1237@gmail.com>
2026-01-12 22:33:49 +0900

    fix: QueryDSL의 Config가 없어서 생긴 오류 해결

--- src/main/java/com/quickticket/quickticket/domain/account/repository/AccountRepositoryImpl.java
+++ src/main/java/com/quickticket/quickticket/domain/account/repository/AccountRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.quickticket.quickticket.domain.account.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
 
 @RequiredArgsConstructor
 public class AccountRepositoryImpl implements AccountRepositoryCustom {
