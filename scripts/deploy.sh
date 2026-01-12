c2ee922e78d21214d36b8e28d6c0d9955d621a48
김순용 <npt1237@gmail.com>
2026-01-12 21:22:56 +0900

    ci: 깃허브 actions, codeDeploy 설정

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+##!/bin/bash
+
+BUILD_JAR=$(ls /home/ec2-user/app/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+
+echo ">>> build 파일 복사" >> /home/ec2-user/deploy.log
+DEPLOY_PATH=/home/ec2-user/app/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ec2-user/deploy.log
+sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2> /home/ec2-user/deploy_err.log &
