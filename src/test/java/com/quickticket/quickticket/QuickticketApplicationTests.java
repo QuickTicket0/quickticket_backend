8552ebd1e1868e30ab2d375d4869b1e4eb7e5731
김순용 <npt1237@gmail.com>
2026-01-12 21:28:01 +0900

    fix: 임시 test로 인해 배포 안되는 오류 해결

--- src/test/java/com/quickticket/quickticket/QuickticketApplicationTests.java
+++ src/test/java/com/quickticket/quickticket/QuickticketApplicationTests.java
@@ -1,13 +1,7 @@
 package com.quickticket.quickticket;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuickticketApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
