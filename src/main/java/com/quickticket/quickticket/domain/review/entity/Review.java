6227550465b9474dca1d1c4db9bf57f845f16ae9
김순용 <npt1237@gmail.com>
2026-01-13 17:16:55 +0900

    feat: Review 엔티티 구현

--- src/main/java/com/quickticket/quickticket/domain/review/entity/Review.java
+++ src/main/java/com/quickticket/quickticket/domain/review/entity/Review.java
@@ -1,4 +1,35 @@
 package com.quickticket.quickticket.domain.review.entity;
 
+import com.quickticket.quickticket.domain.event.entity.Event;
+import com.quickticket.quickticket.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "REVIEW")
+@Getter
+@Setter
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED", nullable = false)
+    private Long reviewId;
+
+    @JoinColumn(columnDefinition = "INT UNSIGNED", nullable = false)
+    private Event event;
+
+    @JoinColumn(columnDefinition = "INT UNSIGNED", nullable = false)
+    private User user;
+
+    @Column(columnDefinition = "TINYINT UNSIGNED", nullable = false)
+    private byte userRating;
+
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 }
