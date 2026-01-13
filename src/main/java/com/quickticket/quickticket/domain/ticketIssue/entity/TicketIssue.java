17dfeb25c1c4a7f99b9da1445d22c538ea650b93
김순용 <npt1237@gmail.com>
2026-01-13 16:32:55 +0900

    chore: TicketIssue 엔티티에 주석 추가

--- src/main/java/com/quickticket/quickticket/domain/ticketIssue/entity/TicketIssue.java
+++ src/main/java/com/quickticket/quickticket/domain/ticketIssue/entity/TicketIssue.java
@@ -9,12 +9,16 @@ import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+// DB의 복합 primary key는 JPA에서 @IdClass 혹은 @EmbeddedId로 구현할 수 있습니다
+// @EmbeddedId는 키로 작용하는 필드들의 묶음 객체로 나타내서 다룰 필요가 있을 때,
+// @IdClass는 복합 키 묶음을 객체로 표현해서 지목하는 용도는 필요없고, 관련된 여러 필드를 각각
+// 다룰 일이 있을때 사용합니다.
 @Entity
 @Table(name = "TICKET_ISSUE")
 @IdClass(TicketIssueId.class)
 @Getter
 @Setter
-public class TIcketIssue {
+public class TicketIssue {
     @Id
     @ManyToOne
     @JoinColumn(name = "user_id", columnDefinition = "INT UNSIGNED", nullable = false)
