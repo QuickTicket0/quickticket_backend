1edf07b907e118c329a856c1d32008ee92f15d83
김순용 <npt1237@gmail.com>
2026-01-13 17:18:06 +0900

    feat: Category 엔티티 구현

--- src/main/java/com/quickticket/quickticket/domain/category/entity/Category.java
+++ src/main/java/com/quickticket/quickticket/domain/category/entity/Category.java
@@ -1,4 +1,19 @@
 package com.quickticket.quickticket.domain.category.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "CATEGORY")
+@Getter
+@Setter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED", nullable = false)
+    private Long categoryId;
+
+    @Column(columnDefinition = "VARCHAR(20)", length = 20, nullable = false)
+    private String name;
 }
