f22aaf40a7028cd8057b42fae0a06f24543eea21
김순용 <npt1237@gmail.com>
2026-01-13 16:32:04 +0900

    feat: CreditTransaction 엔티티 구현

---
+++ src/main/java/com/quickticket/quickticket/domain/creditTransaction/entity/CreditTransaction.java
@@ -0,0 +1,31 @@
+package com.quickticket.quickticket.domain.creditTransaction.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "CREDIT_TRANSACTION")
+@Getter
+@Setter
+public class CreditTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED", nullable = false)
+    private Long creditTransactionId;
+
+    private TransactionType transactionType;
+
+    @Column(nullable = false)
+    private int changeAmount;
+
+    @Column(columnDefinition = "INT UNSIGNED", nullable = false)
+    private long balanceAfter;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
