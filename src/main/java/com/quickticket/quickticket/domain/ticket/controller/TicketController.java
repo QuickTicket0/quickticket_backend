e3dabda2b8d7e348e509040d2d707bdc99b3a1d0
조민지 <sjg04142@naver.com>
2026-01-20 10:14:35 +0900

    feat: 예매 성공 및 취소 페이지 타임리프 데이터 바인딩 적용

--- src/main/java/com/quickticket/quickticket/domain/ticket/controller/TicketController.java
+++ src/main/java/com/quickticket/quickticket/domain/ticket/controller/TicketController.java
@@ -1,29 +1,57 @@
 package com.quickticket.quickticket.domain.ticket.controller;
 
+import com.quickticket.quickticket.domain.event.domain.AgeRating;
+import com.quickticket.quickticket.domain.event.dto.EventResponse;
+import com.quickticket.quickticket.domain.location.dto.LocationCommonDto;
+import com.quickticket.quickticket.domain.payment.seatPayment.dto.SeatPaymentResponse;
+import com.quickticket.quickticket.domain.ticket.domain.TicketStatus;
+import com.quickticket.quickticket.domain.ticket.dto.TicketResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class TicketController {
     @GetMapping("/ticketSuccess")
-    public String ticketSuccess(Model model) { return "ticketSuccess"; }
+    public String ticketSuccess(Model model) {
+
+        // 모델에 각각 정보를 넘긴다고 가정함(콘서트정보, 티켓정보, 결제정보)
+        //  model.addAttribute("event", eventDto);
+        //  model.addAttribute("ticket", ticketDto);
+        //  model.addAttribute("payment", paymentDto);
+        return "ticketSuccess"; }
 
     @GetMapping("/cancelTicketSuccess")
     public String cancelTicketSuccess(Model model) {
+
+        // 모델에 각각 정보를 넘긴다고 가정함(콘서트정보, 티켓정보, 결제정보)
+        //  model.addAttribute("event", eventDto);
+        //  model.addAttribute("ticket", ticketDto);
+        //  model.addAttribute("payment", paymentDto);
         return "cancelTicketSuccess";
     }
 
     @GetMapping("/myPage/tickets")
     public String myTickets(Model model) {
+
+        //ticketList를 리스트 형태로 넘겼다고 가정함
         return "myPage/myTickets";
     }
 
     @GetMapping("/myTicket/{ticketId}")
     public String myTicket(Model model, @PathVariable String ticketId) {
+
+        // 모델에 각각 정보를 넘긴다고 가정함(콘서트정보, 티켓정보, 결제정보, 좌석별가격정보-리스트형태)
+        //  model.addAttribute("event", eventDto);
+        //  model.addAttribute("ticket", ticketDto);
+        //  model.addAttribute("payment", paymentDto);
+        //  model.addAttribute("seatInfoList", seatInfoList);
         return "myPage/myTicket";
     }
 
