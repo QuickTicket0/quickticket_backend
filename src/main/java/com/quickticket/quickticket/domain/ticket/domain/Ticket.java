8a7d080ea795b0fff85ed3c4c59917d959435986
김순용 <npt1237@gmail.com>
2026-01-20 13:13:56 +0900

    chore: Ticket 도메인 객체에 createdAt, canceledAt 속성 추가

--- src/main/java/com/quickticket/quickticket/domain/ticket/domain/Ticket.java
+++ src/main/java/com/quickticket/quickticket/domain/ticket/domain/Ticket.java
@@ -5,6 +5,7 @@ import com.quickticket.quickticket.domain.performance.domain.Performance;
 import com.quickticket.quickticket.domain.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
+import java.time.LocalDateTime;
 
 /// 한 Performance에 대해 사용자가 여러개의 좌석을 선택하고 선착순을 기다리는 예매 정보
 ///
@@ -20,6 +21,8 @@ public class Ticket {
     private TicketStatus status;
     private Long waitingNumber;
     private Integer personNumber;
+    private LocalDateTime createdAt;
+    private LocalDateTime canceledAt;
 
     /// 반드시 create로 생성된 객체가 DB에 할당되었을 상황에만 호출하세요
     public void assignId(Long id) {
