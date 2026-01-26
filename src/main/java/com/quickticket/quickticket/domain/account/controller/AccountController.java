ff380f005a53f5160a166501230cdb58a0f4fc3f
김순용 <npt1237@gmail.com>
2026-01-26 15:33:48 +0900

    feat: user 도메인객체에 UserDetails 상속

--- src/main/java/com/quickticket/quickticket/domain/account/controller/AccountController.java
+++ src/main/java/com/quickticket/quickticket/domain/account/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.quickticket.quickticket.domain.account.dto.AccountRequest;
 import com.quickticket.quickticket.domain.user.domain.User;
 import com.quickticket.quickticket.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,7 @@ public class AccountController {
     }
 
     @PostMapping("/api/account/logout")
-    public void logout() {
+    public void logout(@AuthenticationPrincipal User user) {
 
     }
 
--- src/main/java/com/quickticket/quickticket/domain/user/domain/User.java
+++ src/main/java/com/quickticket/quickticket/domain/user/domain/User.java
@@ -3,14 +3,19 @@ package com.quickticket.quickticket.domain.user.domain;
 import com.quickticket.quickticket.domain.account.domain.AccountType;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 /// 비밀번호나 인증 등은 account 도메인에서 관할하고, user 도메인은 사용자 데이터 처리에 집중합니다.
 @Builder
 @Getter
-public class User {
+public class User implements UserDetails {
     private Long id;
     private String username;
     private String realName;
@@ -23,6 +28,21 @@ public class User {
     private Long credit = 0L;
     private AccountType type;
     private LocalDateTime createdAt;
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
 
     /// 반드시 create로 생성된 객체가 DB에 할당되었을 상황에만 호출하세요
     public void assignId(Long id) {
