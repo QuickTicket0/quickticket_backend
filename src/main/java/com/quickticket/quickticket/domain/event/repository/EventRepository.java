20487d72a80c76a43a1adbbc87505ca4fccb0e0c
lavi27 <62757958+lavi27@users.noreply.github.com>
2026-01-31 11:19:16 +0900

    Update EventRepository.java

--- src/main/java/com/quickticket/quickticket/domain/event/repository/EventRepository.java
+++ src/main/java/com/quickticket/quickticket/domain/event/repository/EventRepository.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
     Optional<EventEntity> getById(Long id);
+    
+    Optional<AgeRating> getAgeRatingById(Long id);
 
     List<EventEntity> getAllByOrderByViewsDesc(Pageable pageable);
 }
