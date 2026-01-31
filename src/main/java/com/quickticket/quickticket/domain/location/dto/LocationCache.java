9c8e82f4dc648052a6ffd0f2c951d2efd76fb793
lavi27 <62757958+lavi27@users.noreply.github.com>
2026-01-31 11:29:56 +0900

    Create LocationCache.java

---
+++ src/main/java/com/quickticket/quickticket/domain/location/dto/LocationCache.java
@@ -0,0 +1,11 @@
+public record LocationCommonDto implements Serializable(
+    Long id,
+    String name,
+    String zipNumber,
+    String sido,
+    String siGunGu,
+    String eupMyun,
+    String doroCode,
+    String doroName,
+    String phone
+) {}
