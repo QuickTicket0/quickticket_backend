3348fc427a643bf96a45042cd47b406a1fc96c37
lavi27 <62757958+lavi27@users.noreply.github.com>
2026-01-31 10:36:13 +0900

    Create AopForTransaction.java

---
+++ src/main/java/com/quickticket/quickticket/shared/aops/AopForTransaction.java
@@ -0,0 +1,11 @@
+/**
+ * AOP에서 트랜잭션 분리를 위한 클래스
+ */
+@Component
+public class AopForTransaction {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
+    }
+}
