84b7526e6791c6a7ef4628a49d0a47658685fbf2
김순용 <npt1237@gmail.com>
2026-02-04 23:13:48 +0900

    fix: 분산 락과 Transactional의 실행 순서 적용

---
+++ src/main/java/com/quickticket/quickticket/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.quickticket.quickticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement(order = Ordered.LOWEST_PRECEDENCE)
+public class AppConfig {
+}
