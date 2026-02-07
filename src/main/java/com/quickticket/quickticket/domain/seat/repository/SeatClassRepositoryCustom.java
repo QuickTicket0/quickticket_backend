027b4b1b84fb9995ebfda9ced20513a47e929838
lavi27 <62757958+lavi27@users.noreply.github.com>
2026-02-07 13:16:35 +0900

    Update SeatClassRepositoryCustom.java

--- src/main/java/com/quickticket/quickticket/domain/seat/repository/SeatClassRepositoryCustom.java
+++ src/main/java/com/quickticket/quickticket/domain/seat/repository/SeatClassRepositoryCustom.java
@@ -4,6 +4,6 @@ import com.quickticket.quickticket.domain.seat.dto.SeatClassCache;
 import org.springframework.cache.annotation.Cacheable;
 
 public interface SeatClassRepositoryCustom {
-    @Cacheable(value = "seatClass_cacheDto", key = "#seatClassId:#eventId")
+    @Cacheable(value = "cache:seat-class-cache", key = "#seatClassId:#eventId")
     SeatClassCache getCacheById(Long seatClassId, Long eventId);
 }
