fb425c84bf6e8046d417c12656e18d8d3b01459b
lavi27 <62757958+lavi27@users.noreply.github.com>
2026-02-07 12:39:22 +0900

    Update TicketIssueBulkinsertRepositoryImpl.java

--- src/main/java/com/quickticket/quickticket/domain/ticket/repository/TicketIssueBulkinsertRepositoryImpl.java
+++ src/main/java/com/quickticket/quickticket/domain/ticket/repository/TicketIssueBulkinsertRepositoryImpl.java
@@ -2,26 +2,37 @@
 @RequiredArgsConstructor
 public class TicketIssueBulkinsertRepositoryImpl implements TicketIssueBulkinsertRepository {
     private static int EVENT_BUFFER_SIZE = 100;
-  
+    
+    private final RedisAtomicLong ticketIssueIdGenerator;
+    private final RedisTemplate redisTemplate;
+    private final RabbitTemplate rabbitTemplate;
+    private final TicketIssueMapper ticketIssueMapper;
+    private final @Lazy TicketIssueRepository ticketIssueRepository;
     private final List<TicketInsertEvent> eventBuffer = new List<>();
-  
+
     public Ticket saveDomainForBulk(Ticket domain) {
-        if (domain.id != null) {
-            // insert할 필요가 없음
-            // 일반 메서드 호출?
+        if (domain.getId() != null) {
+            ticketIssueRepository.saveDomain(domain);
+            return;
         }
-      
-        this.eventBuffer.push();
-        // redis 삽입
-        // 삽입시 변경되는 필드 적용
-        this.flushBuffer();
+
+        var entity = ticketIssueMapper.toEntity(domain);
+        entity.id = ticketIssueIdGenerator.incrementAndGet();
+
+        redisTemplate.opsForValue().set("temp:ticketIssue:" + entity.id, entity);
+        
+        this.addToBuffer(new TicketInsertEvent(entity));
+        
+        return ticketIssueMapper.toDomain(entity);
     }
 
-    private void flushBuffer() {
+    private void addToBuffer(TicketInsertEvent event) {
+        this.eventBuffer.push(event);
+        
         if (this.eventBuffer.length() < EVENT_BUFFER_SIZE) {
             return;
         }
 
-        // rabbitmq로 전송
+        rabbitTemplate.convertAndSend("worker.bulkinsert.exchange", "worker.bulkinsert.ticket", eventBuffer);
     }
 }
