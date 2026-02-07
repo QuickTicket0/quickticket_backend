0ea705886c29727555be8383e4740b642d5dc174
lavi27 <62757958+lavi27@users.noreply.github.com>
2026-02-07 13:17:09 +0900

    Update SeatAreaRepositoryCustom.java

--- src/main/java/com/quickticket/quickticket/domain/seat/repository/SeatAreaRepositoryCustom.java
+++ src/main/java/com/quickticket/quickticket/domain/seat/repository/SeatAreaRepositoryCustom.java
@@ -4,6 +4,6 @@ import com.quickticket.quickticket.domain.seat.dto.SeatAreaCache;
 import org.springframework.cache.annotation.Cacheable;
 
 public interface SeatAreaRepositoryCustom {
-    @Cacheable(value = "seatArea_cacheDto", key = "#seatAreaId:#eventId")
+    @Cacheable(value = "cache:seat-area-cache", key = "#seatAreaId:#eventId")
     SeatAreaCache getCacheById(Long seatAreaId, Long eventId);
 }
